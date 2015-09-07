e54389bc31b501160b7e905dc077891e7d6c4fd5
Antonio Espinosa <antespi@gmail.com>
2015-09-07 10:30:37 +0200

    Changes on Antiun addons

---
+++ sample_addon_antiun/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# See README.rst file on addon root folder for license details
+
+{
+    'name': "Sample Addon",
+    'category': 'Personalization',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'base',
+    ],
+    'external_dependencies': {},
+    'data': [
+    ],
+    'qweb': [
+    ],
+    'js': [
+    ],
+    'author': 'Antiun Ingeniería S.L.',
+    'website': 'http://www.antiun.com',
+    'license': 'AGPL-3',
+    'demo': [],
+    'test': [],
+    'installable': True,
+    # 'auto_install':False,
+    # 'application':False,
+}
